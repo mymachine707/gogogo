761456ada7ff9cded30fd927e3355ad284a2efe3
@my_machine_707_Dell707 <mymaxs441@gmail.com>
2022-09-13 12:19:48 +0500

    add commit

--- matrixMultiplication/test.go
+++ matrixMultiplication/test.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//function
 func main() {
 	arr1 := [2][2]float64{{1, 2}, {3, 4}}
 	arr2 := [2][2]float64{{5, 6}, {7, 8}}
@@ -11,7 +12,6 @@ func main() {
 		ans[i] = make([]float64, 2)
 		for s := 0; s < len(arr1); s++ {
 			ans[i][s] = 0
-
 			for p := 0; p < len(arr1); p++ {
 				ans[i][s] += arr1[i][p] * arr2[p][s]
 				fmt.Printf("ans[%d][%d]+=%0.0f*%0.0f\n", i, s, arr1[i][p], arr2[p][s])
