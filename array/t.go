00064e8bddbe9d0d6e0a1f633965febfa2ec646f
@my_machine_707_Dell707 <mymaxs441@gmail.com>
2022-09-13 15:18:20 +0500

    s

---
+++ array/t.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var n int
+	var m int
+
+	fmt.Println("n=")
+	fmt.Scanf("%d", &n)
+	fmt.Println("")
+	fmt.Println("m=")
+	fmt.Scanf("%d", &m)
+
+	fmt.Printf("n=%d\tm=%d", n, m)
+}
