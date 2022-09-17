ebb98b548c7da0063a27e7d56f53a77a3ee61c7d
mymachine707 <mymaxs441@gmail.com>
2022-09-17 17:30:39 +0500

    add and minus

---
+++ newProject/minus/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("Sonlarni string holatida qo'shish")
+	var num1 string = "5465" // o'n ming
+	var num2 string = "0001" // o'n beshming
+
+	fmt.Printf("%s", "\tEntered number 1: ")
+	fmt.Scanf("%s", &num1)
+
+	fmt.Printf("%s", "\tEntered number 2: ")
+	fmt.Scanf("%s", &num2)
+
+	a := strings.Split(num1, "")
+	b := strings.Split(num2, "")
+
+	// kiruvchi stringlarni uzunligini tenglashtirish======================
+	var answer string // javobni answerga biriktirish
+	var s []string
+	var y int
+	var e int // a katta b dan
+	var c int // a kichik b dan
+	// qaysi son kattaligini tekshirish kerak
+	if len(a) > len(b) {
+		// a katta b dan
+		e += 1
+	} else if len(a) < len(b) {
+		// a kichik b dan
+		c += 1
+	} else if len(a) == len(b) {
+		// a teng b ga
+		for i := 0; i < len(a); i++ {
+			if a[i] > b[i] {
+				// a katta b dan
+				e += 1
+				break
+			} else if a[i] < b[i] {
+				// a kichik b dan
+				c += 1
+				break
+			}
+		}
+		answer = "0"
+
+	}
+
+	// slicelarni bir biriga tenglashtirish jarayoni bu kod o'zgartirilmasin
+
+	if e == 1 { // a katta b dan
+		y = len(a) - len(b)
+		for i := 0; i < y; i++ {
+			s = append(s, "0")
+		}
+		s = append(s, b...)
+		b = s
+		s = nil
+
+	} else if c == 1 { // a kichik b dan
+		y = len(b) - len(a)
+		for i := 0; i < y; i++ {
+			s = append(s, "0")
+		}
+		s = append(s, a...)
+		a = s
+		s = nil
+	}
+
+	if e == 1 { // a>b
+		for i := 0; i < len(a); i++ {
+			answer += intconvstr(strconvInt(a[i]) - strconvInt(b[i]))
+		}
+		fmt.Println("\tnumber1 > number2")
+		fmt.Printf("\tAnswer number: %s\n", answer)
+	} else if c == 1 { // a<b
+		for i := 0; i < len(a); i++ {
+			answer += intconvstr(strconvInt(b[i]) - strconvInt(a[i]))
+		}
+		fmt.Println("\tnumber1 < number2")
+		fmt.Printf("\tAnswer number: %s\n", answer)
+	}
+
+} //------------------------------------------------main yopildi
+
+// stringdan intga o'tqizadi
+func strconvInt(str string) int {
+	intVar, err := strconv.Atoi(str)
+	if err != nil {
+		return 0 // sho'tda tanish bilishchili bo'b ketti err bersa 0 qayatrmasligi kere boshqa javob so'ra
+	}
+	return intVar
+}
+
+// intdan stringga o'tkizadi
+
+func intconvstr(num int) string {
+	str := strconv.Itoa(num)
+	return str
+}
