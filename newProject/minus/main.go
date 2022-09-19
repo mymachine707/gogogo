6a0ff779b3dcde86049d7515510a6cafdf5639e9
@my_machine_707_Dell707 <mymaxs441@gmail.com>
2022-09-19 11:19:47 +0500

    news minus.go

--- newProject/minus/main.go
+++ newProject/minus/main.go
@@ -9,17 +9,17 @@ import (
 func main() {
 
 	fmt.Println("Sonlarni string holatida ayirish")
-	var num1 string = "1000888880000000000000000000000000000000000000000000000000000000000000000007" // o'n ming
-	var num2 string = "890404040404040517159"                                                        // o'n beshming
-	/*
-		fmt.Printf("%s", "\tEntered number 1: ")
-		fmt.Scanf("%s", &num1)
+	var num1 int
+	var num2 int
 
-		fmt.Printf("%s", "\tEntered number 2: ")
-		fmt.Scanf("%s", &num2)
-	*/
-	a := strings.Split(num1, "")
-	b := strings.Split(num2, "")
+	fmt.Printf("%s", "\tEntered number 1: ")
+	fmt.Scanf("%d", &num1)
+
+	fmt.Printf("%s", "\tEntered number 2: ")
+	fmt.Scanf("%d", &num2)
+
+	a := strings.Split(int_str(num1), "")
+	b := strings.Split(int_str(num2), "")
 
 	// kiruvchi stringlarni uzunligini tenglashtirish======================
 
@@ -82,7 +82,7 @@ func main() {
 
 	//a bilan b berilgan slice holatida
 	if e == 1 {
-		fmt.Println("number1 > number 2")
+		fmt.Println("\nnumber1 > number 2")
 		// a > b bo'lsa:
 		for i := len(a); i > 0; i-- {
 			if str_Int(a[i-1])-str_Int(b[i-1]) > 0 { // Agar joriy indekslar ayirmasi musbat bo'lsa
@@ -114,6 +114,38 @@ func main() {
 
 		}
 
+	} else if c == 1 {
+		fmt.Println("\nnumber1 < number 2")
+		// a < b bo'lsa:
+		for i := len(b); i > 0; i-- {
+			if str_Int(b[i-1])-str_Int(a[i-1]) > 0 { // Agar joriy indekslar ayirmasi musbat bo'lsa
+				z[i-1] = int_str(str_Int(b[i-1]) - str_Int(a[i-1])) // z joriy indeksiga ayirma qiymati beriladi
+			} else if str_Int(b[i-1])-str_Int(a[i-1]) == 0 { // Agar joriy indekslar ayirmasi 0 ga teng bo'lsa
+				z[i-1] = int_str(0) // z joriy indeksiga 0 qiymat beriladi
+			} else if str_Int(b[i-1])-str_Int(a[i-1]) < 0 { // agar joriy indekslar ayirmasi 0 dan kichik bo'lsa
+				b[i-1] = int_str(str_Int(b[i-1]) + 10)              // katt sonning joriy indeksiga 10 soni qo'shiladi va qaytadan yenglashtiriladi
+				z[i-1] = int_str(str_Int(b[i-1]) - str_Int(a[i-1])) // z ning joriy qiymatini sonlarning joriy qiymatlari ayirmasiga tenglashtiriladi va...
+				if str_Int(b[i-2]) > 0 {                            // agar katta sonning joriy qiymatidan bitta oldingi qiymati 0 dan katta bo'lsa
+					b[i-2] = int_str(str_Int(b[i-2]) - 1) // katta sonning joriy qiymatidan bitta oldingi sonnini bittaga kamaytiriladi
+				} else if str_Int(b[i-2]) == 0 { // agar katta sonning joriy qiymatidan bitta oldingi qiymati 0 ga teng bo'lsa...
+					b[i-2] = "9"           // katta sonning joriy qiymatidan bitta oldingi sonnini 9 soniga tenglashtiriladi
+					var boolen bool = true // for loop uchun boolen qiyamtini true holatida ochildi
+					var m int = 3          // indekslarni tekshirish uchun m int shaklida ochildi va katta sonning joriy qiymatidan ikkita oldingi qiymatini tekshirish uchun m sonini 3 ga tenglandi
+					for boolen {           // for loop ochildi
+						if str_Int(b[i-m]) > 0 { // Agar katta sonning joriy indeksidan ikkita oldingi indeksi 0 dan katta bo'lsa
+							b[i-m] = int_str(str_Int(b[i-m]) - 1) // katta sonning joriy indeksidan ikkita oldingi indeksidan 1 soni ayiriladi
+							boolen = false                        // boolen false qiymatini oladi
+							break                                 // for loopdan chiqiladi
+						} else if str_Int(b[i-m]) == 0 { // Agar katta sonning joriy indeksidan ikkita oldingi indeksi 0 ga teng bo'lsa
+							b[i-m] = "9" // Katta sonning joriy indeksidan ikkita oldingi indeksi "9" ga tenglanadi
+							m++          // m soni 1 ga oshiriladi va for loop qaytadan tekshiruvni boshlaydi
+						}
+
+					}
+				}
+			}
+
+		}
 	}
 	//natijani birlashtirish
 	var answers string
--- newProject/test/main.go
+++ newProject/test/main.go
@@ -1,16 +1,31 @@
-package main
+fmt.Println("number1 < number 2")
+// a > b bo'lsa:
+for i := len(a); i > 0; i-- {
+	if str_Int(a[i-1])-str_Int(b[i-1]) > 0 { // Agar joriy indekslar ayirmasi musbat bo'lsa
+		z[i-1] = int_str(str_Int(a[i-1]) - str_Int(b[i-1])) // z joriy indeksiga ayirma qiymati beriladi
+	} else if str_Int(a[i-1])-str_Int(b[i-1]) == 0 { // Agar joriy indekslar ayirmasi 0 ga teng bo'lsa
+		z[i-1] = int_str(0) // z joriy indeksiga 0 qiymat beriladi
+	} else if str_Int(a[i-1])-str_Int(b[i-1]) < 0 { // agar joriy indekslar ayirmasi 0 dan kichik bo'lsa
+		a[i-1] = int_str(str_Int(a[i-1]) + 10)              // katt sonning joriy indeksiga 10 soni qo'shiladi va qaytadan yenglashtiriladi
+		z[i-1] = int_str(str_Int(a[i-1]) - str_Int(b[i-1])) // z ning joriy qiymatini sonlarning joriy qiymatlari ayirmasiga tenglashtiriladi va...
+		if str_Int(a[i-2]) > 0 {                            // agar katta sonning joriy qiymatidan bitta oldingi qiymati 0 dan katta bo'lsa
+			a[i-2] = int_str(str_Int(a[i-2]) - 1) // katta sonning joriy qiymatidan bitta oldingi sonnini bittaga kamaytiriladi
+		} else if str_Int(a[i-2]) == 0 { // agar katta sonning joriy qiymatidan bitta oldingi qiymati 0 ga teng bo'lsa...
+			a[i-2] = "9"           // katta sonning joriy qiymatidan bitta oldingi sonnini 9 soniga tenglashtiriladi
+			var boolen bool = true // for loop uchun boolen qiyamtini true holatida ochildi
+			var m int = 3          // indekslarni tekshirish uchun m int shaklida ochildi va katta sonning joriy qiymatidan ikkita oldingi qiymatini tekshirish uchun m sonini 3 ga tenglandi
+			for boolen {           // for loop ochildi
+				if str_Int(a[i-m]) > 0 { // Agar katta sonning joriy indeksidan ikkita oldingi indeksi 0 dan katta bo'lsa
+					a[i-m] = int_str(str_Int(a[i-m]) - 1) // katta sonning joriy indeksidan ikkita oldingi indeksidan 1 soni ayiriladi
+					boolen = false                        // boolen false qiymatini oladi
+					break                                 // for loopdan chiqiladi
+				} else if str_Int(a[i-m]) == 0 { // Agar katta sonning joriy indeksidan ikkita oldingi indeksi 0 ga teng bo'lsa
+					a[i-m] = "9" // Katta sonning joriy indeksidan ikkita oldingi indeksi "9" ga tenglanadi
+					m++          // m soni 1 ga oshiriladi va for loop qaytadan tekshiruvni boshlaydi
+				}
 
-import "fmt"
-
-func main() {
-	var m int
-	var x bool = true
-	for x {
-		if m == 100 {
-			x = false
+			}
 		}
-		fmt.Println(m)
-		m++
 	}
-	fmt.Println("The end")
+
 }
