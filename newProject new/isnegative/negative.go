530fd78a6b37b61c89dc8908969c5b2a3db4f23c
mymachine707 <mymaxs441@gmail.com>
2022-09-20 14:34:12 +0500

    news

--- newProject new/isnegative/negative.go
+++ newProject new/isnegative/negative.go
@@ -14,30 +14,24 @@ func main() {
 
 	fmt.Printf("%s", "\tEntered number 1: ")
 	fmt.Scanf("%s", &num)
-	//
+	isnegatives(num)
+}
+func isnegatives(num string) bool {
 	a := strings.Split(num, "")
 	//
-	var number bool
-	//
-	var first string = a[0]
-	//
-	for i := 1; i < len(a); i++ {
-		if isInt(a[i]) {
-			number = true
-		} else {
-			number = false
-			return number
-		}
+	var negative bool
+
+	if a[0] == "+" {
+		negative = false
+	} else if a[0] == "-" {
+		negative = true
+	} else if isInt(a[0]) {
+		negative = false
+	} else {
+		panic("This string is not a number!")
 	}
-	/*
-		fmt.Println("This number is negative = ", negative)
-		fmt.Println("The first index of string is number = ", number)
-	*/
-	//
-	//
-
+	return negative
 }
-
 func isInt(s string) bool {
 	for _, c := range s {
 		if !unicode.IsDigit(c) {
